007e9a29206cdc0b66946b61d2ebeb12965f2473
Eddy Erazo Zamudio <eerazozamudio@gmail.com>
2019-07-16 11:25:19 -0500

    Subir a Github

--- Deploy.sh
+++ Deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sencha app build
 cd build/production/
-zip -r ExtMVC.zip ExtMVC.zip 
+zip -r ExtMVC.zip ExtMVC
 scp -p ExtMVC.zip root@198.211.99.171:/var/www/html/vmt
 ssh root@198.211.99.171
